e0e60be7461b55eb1e3b5f6b05ae84420aa096f5
José Hildebrando <jose.hildebrando@gmail.com>
2021-03-31 17:05:45 -0300

    Create knntest, upd knn

--- knn.py
+++ knn.py
@@ -9,20 +9,21 @@ class KNN:
     def __init__(self, k=3):
         self.k = k
 
-    def fit(X, y):
+    def fit(self, X, y): # fit the training samples
         self.X_train = X
         self.y_train = y
 
-    def predict(self, X):
-        predict_labels = [self._predict(x) for x in X]
-        return np.array(predict_labels)
+    def predict(self, X):              # capital X means multiple samples
+        y_pred = [self._predict(x) for x in X]
+        return np.array(y_pred)
 
-    def _predict(self, x):
+    def _predict(self, x):             # simple example
         # compute distances
         distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
         # get k nearest samples, labels
         k_indices = np.argsort(distances)[:self.k]
-        k_nearest_labels = [self.y_train[i] for i in k_indices]
+        # Extract the labels of the k nearest neighbor training samples
+        k_neighbor_labels = [self.y_train[i] for i in k_indices]
         # majority vote, most common label
-        most_common = Counter(k_nearest_labels).most_common(1)
+        most_common = Counter(k_neighbor_labels).most_common(1)
         return most_common[0][0]
